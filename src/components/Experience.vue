b3526687378a8b78426e288a3a4733458ddfb5f7
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-07-15 04:44:51 +0100

    added experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -10,7 +10,29 @@
       Experiences
     </p>
 
-  
+    <p class="font-semibold text-xl xl:text-2xl hover:text-pink-700 hover:cursor-pointer"><a href="https://ardaworks.id/" target="_blank" rel="noopener noreferrer">Nocxa</a></p>
+    <ol class="relative border-l border-gray-200">
+      <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+          <time class="text-sm font-normal leading-none text-gray-400 xl:text-lg">Septemeber 2020 - Present</time>
+          <div class="bg-black bg-opacity-20 rounded-lg p-4 xl:text-lg mt-2 z-20">
+            <h3 class="text-lg font-semibold text-yellow-500 xl:text-xl mb-3">Front-end Developer</h3>
+            <ul class="lg:text-lg xl:text-xl">• Designing and developing websites and web apps for clients.</ul>
+            <h3 class="mt-4 font-semibold text-yellow-500 mb-2">Related Techstacks</h3>
+            <div class="flex gap-x-4 flex-wrap gap-y-2 pb-2">
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">HTML</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">CSS</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">JS</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">Django</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">Vue.js</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">jQuery</button>
+              <button class="border px-4 lg:px-6 lg:py-2 transition-colors text-sm border-white hover:border-pink-700 hover:bg-pink-700">Bootstrap</button>
+            </div>
+          </div>
+      </li>
+    </ol>
+
+   
     <img src="/experience/sphere.svg" alt="Round" class="absolute hidden lg:block -z-0 bottom-60 -left-24 w-44 xl:w-64 2xl:-left-12 opacity-30">
     <img src="/experience/star.svg" alt="Star" class="absolute hidden lg:block -z-0 top-8 left-0 w-64 opacity-30">
     <img src="/experience/square.svg" alt="Square" class="absolute hidden lg:block w-64 -z-0 opacity-30 -right-40 top-80 xl:-right-24 2xl:w-72 2xl:-right-40">
