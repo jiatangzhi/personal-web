69aa6d82ad0c7b39b14f79bfe013422680e66c51
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-09-13 08:27:45 +0200

    updated location

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,7 +9,7 @@
             Curiosity, Breakthroughs, and Limitless Ideas
           </p>
           <p class="pt-8 pb-4 md:pr-64 lg:pr-72 xl:pr-96 md:text-lg lg:text-lg md:pt-12 md:pb-10 xl:text-2xl 2xl:pt-22 2xl:pr-[550px] z-20">
-             Based in London, UK. I'm currently seeking new opportunities in front-end development. I'm open to work hybrid or remotely and have flexible hours.
+             Based in Paris, France. I'm currently seeking new opportunities in front-end development. I'm open to work hybrid or remotely and have flexible hours.
           </p>
           <a href="/#about">
             <button class="bg-pink-500 px-6 py-2 md:text-lg lg:text-lg hover:bg-pink-700 transition-all md:px-8 md:py-3 xl:text-xl 2xl:text-2xl font-semibold">Discover More</button>
